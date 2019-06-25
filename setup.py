2003276198fe321378357ec550c7314aeee1a0a7
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2019-06-25 10:37:40 +0200

    Version 1.2.1b0

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from os.path import dirname, abspath
+'from os.path import dirname, abspath
 
 from setuptools import find_packages, setup
 
@@ -8,7 +8,7 @@ README = open('README.md', encoding='utf-8').read()
 
 setup(
     name='django-reactor',
-    version='1.2.0b0',
+    version='1.2.1b0',
     url='https://github.com/edelvalle/reactor',
     author='Eddy Ernesto del Valle Pino',
     author_email='eddy@edelvalle.me',
