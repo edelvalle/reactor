04966a27d703190f7bace442514a885cf55415f6
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2020-08-10 09:56:59 +0200

    Remove unnecessary noqa markers

--- reactor/asgi.py
+++ reactor/asgi.py
@@ -5,10 +5,10 @@ django.setup(set_prefix=False)
 
 
 def get_asgi_application():
-    from django.conf import settings  # noqa
-    from django.utils.module_loading import import_module  # noqa
-    from channels.auth import AuthMiddlewareStack # noqa
-    from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
+    from django.conf import settings
+    from django.utils.module_loading import import_module
+    from channels.auth import AuthMiddlewareStack
+    from channels.routing import ProtocolTypeRouter, URLRouter
 
     urls = import_module(settings.ROOT_URLCONF)
     return ProtocolTypeRouter({
