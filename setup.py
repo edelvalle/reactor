6f2ec7867e9a79ee0fdac4f92647beac7962986d
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2020-08-25 15:40:27 +0200

    Version 2.1.3b0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open('README.md', encoding='utf-8').read()
 
 setup(
     name='django-reactor',
-    version='2.1.2b0',
+    version='2.1.3b0',
     url='https://github.com/edelvalle/reactor',
     author='Eddy Ernesto del Valle Pino',
     author_email='eddy@edelvalle.me',
