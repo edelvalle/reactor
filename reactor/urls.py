08ea6f2c9c728263278e911aa0301515f32a5816
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2021-11-11 21:05:11 +0100

    Remove HTTP handlers

--- reactor/urls.py
+++ reactor/urls.py
@@ -1,34 +1,5 @@
-import json
-
-from django.http.response import HttpResponse
 from django.urls import path
 
-from .component import Component
 from .consumer import ReactorConsumer
-
-
-def build_component(request, component):
-    component = _get_component(request, component)
-    return HttpResponse(component._render())
-
-
-def user_event(request, component, event):
-    component = _get_component(request, component)
-    args = json.loads(request.GET.get("args", "{}"))
-    html = " ".join(component._dispatch(event, args))
-    return HttpResponse(html)
-
-
-def _get_component(request, component) -> Component:
-    component = Component._build(component, _context={"user": request.user})
-    kwargs = orjson.loads(request.GET.get("state", "{}"))
-    component.mount(**kwargs)
-    return component
-
-
-urlpatterns = [
-    path("__reactor__/<component>", build_component),
-    path("__reactor__/<component>/<event>", user_event),
-]
 
 websocket_urlpatterns = [path("__reactor__", ReactorConsumer.as_asgi())]
